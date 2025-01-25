597293ca3ae7d00e4b6073dacca80be28b0dd7ca
BLooperZ <blooperz@users.noreply.github.com>
2025-01-25 21:20:16 +0200

    Fancy scores

--- player_score.gd
+++ player_score.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
 @export var player_name = "0"
-@onready var label: Label = $Label
+@onready var labels: Control = $Labels
+
+
 
 func _ready() -> void:
 	Manager.connect("score_changed", on_score_changed)
 	
 
 func on_score_changed(score_player_name):
-		label.text = str(Manager.scores[player_name])
+	var text = str(Manager.scores[player_name])
+	for label in labels.get_children():
+		label.text = text
