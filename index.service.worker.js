64dc93b0245056ef53d4c56c74a31ed33ae65673
BLooperZ <bloop93@gmail.com>
2025-01-25 23:38:57 +0000

    Deploying to gh-pages from @ BLooperZ/DiveHard@caab2eebd0ab76f39196169f27f1106d1382a0a6 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737840814|8978466';
+const CACHE_VERSION = '1737848327|9012826';
 /** @type {string} */
 const CACHE_PREFIX = 'DiveHard-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
